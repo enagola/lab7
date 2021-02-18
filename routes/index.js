480ee3d3204886af4d8417b4e4e1a6a695fa1990
Ethan Nagola <enagola@ucsd.edu>
2021-02-18 10:56:18 -0800

    update for heroku

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,22 @@ var projects = require('../projects.json');
 /*
  * GET home page.
  */
+function initializePage() {
+	$('.likeBtn').click(clickLikeBtn);
+}
+
+function clickLikeBtn(e) {
+	e.preventDefault();
+	ga('create', 'UA-164218871-1', 'auto');
+	ga('send', 'event', 'like', 'click');
+}
 
 exports.view = function(request, response){
+	projects['viewAlt'] = false;
   	response.render('index', projects);
+};
+
+exports.viewAlt = function(request, response){
+	projects['viewAlt'] = true;
+	response.render('index', projects);
 };
